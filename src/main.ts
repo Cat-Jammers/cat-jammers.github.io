2743ca0ad23bbee6c78385c4583baa7c04d265c9
DJ_Laser <90146620+DJ-Laser@users.noreply.github.com>
2025-02-22 21:55:19 -0500

    migrate to tailwind

--- src/main.ts
+++ src/main.ts
@@ -4,17 +4,19 @@ import { setupCounter } from "./counter";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
+    <div class="flex flex-nowrap justify-around">
+      <a href="https://vite.dev" target="_blank">
+        <img src="/vite.svg" class="h-30 p-6" alt="Vite logo" />
+      </a>
+      <a href="https://www.typescriptlang.org/" target="_blank">
+        <img src="${typescriptLogo}" class="h-30 p-6 vanilla" alt="TypeScript logo" />
+      </a>
     </div>
-    <p class="read-the-docs">
+    <h1 class="text-5xl">Vite + TypeScript</h1>
+    <div class="p-8">
+      <button id="counter" type="button" class="px-5 py-2 rounded-lg border border-transparent hover:border-cyan-500 bg-neutral-100 dark:bg-neutral-950 cursor-pointer transition-colors"></button>
+    </div>
+    <p class="text-neutral-400">
       Click on the Vite and TypeScript logos to learn more
     </p>
   </div>
