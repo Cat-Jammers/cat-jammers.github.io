305fe316300e29f3151af4c068ecbb739967be23
Alanik <lilia.dostie@socprep.org>
2025-02-25 13:25:43 -0500

    added icons, still need to figure out how to put walking cat image on the right side

--- src/components/searchBar.ts
+++ src/components/searchBar.ts
@@ -5,16 +5,17 @@
 export function searchBar(): string {
   return `
     <div id="mainSearchBar" class="grid grid-rows-2">
-      <div class="flex justify-center row-start-1 col-start-1">
-        neko-search
+      <div class="flex justify-center row-start-1 col-start-1 pb-2">
+        <img src="/Neko Search.png" alt="neko-search" class="w-auto h-auto">
       </div>
       <div class="flex justify-end row-start-1 col-start-1">
         cat
       </div>
-      <div class="p-2 w-[40vw] flex text-center bg-weak-100 border-strong-100 border-5 rounded-full">
-        <p class="mr-4 ">icon</p>
+      <div class="p-2 w-[60vw] flex text-center justify-self-center bg-weak-100 border-strong-100 border-5 rounded-full">
+        
+        <img src="/search.png" alt="search-icon" class="w-auto h-auto pr-2 pl-2">
         <input class="text-strong-100" placeholder="search stuff here..."></input>
-        <p class="ml-auto">icon</p>
+        <img src="/walkingCat.png" alt="walking-cat-icon" class="justify-end w-auto h-auto pr-2 pl-2">
       </div>
     </div>
   `;
