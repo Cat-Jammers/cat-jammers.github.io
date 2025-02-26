fdf8a06c0db0f310aa8e8147f06edfdf51ef45d5
Alanik <lilia.dostie@socprep.org>
2025-02-26 12:27:10 -0500

    added cat sitting on search bar

--- src/components/searchBar.ts
+++ src/components/searchBar.ts
@@ -4,18 +4,19 @@
  */
 export function searchBar(): string {
   return `
-    <div id="mainSearchBar" class="grid grid-rows-2">
+    <div id="mainSearchBar" class="grid grid-rows-2 h-auto">
       <div class="flex justify-center row-start-1 col-start-1 pb-2">
-        <img src="/Neko Search.png" alt="neko-search" class="w-auto h-auto">
+        <img src="/Neko Search.png" alt="neko-search" class="w-auto h-auto object-none px-40">
+        <img src="/Cat.png" alt="neko-search" class="w-auto h-auto">
       </div>
       <div class="flex justify-end row-start-1 col-start-1">
-        cat
-      </div>
-      <div class="p-2 w-[60vw] flex text-center justify-self-center bg-weak-100 border-strong-100 border-5 rounded-full">
         
-        <img src="/search.png" alt="search-icon" class="w-auto h-auto pr-2 pl-2">
+      </div>
+      <div class="p-2 w-[60vw] h-fit flex text-center justify-self-center bg-weak-100 border-strong-100 border-5 rounded-full">
+        
+        <img src="/search.png" alt="search-icon" class="w-auto h-auto pr-2 pl-2 object-none">
         <input class="text-strong-100 w-[50vw]" placeholder="search stuff here..."></input>
-        <img src="/walkingCat.png" alt="walking-cat-icon" class="justify-end w-auto h-auto pr-2 pl-2">
+        <img src="/walkingCat.png" alt="walking-cat-icon" class="justify-end w-auto h-auto pr-2 pl-2 object-none">
       </div>
     </div>
   `;
